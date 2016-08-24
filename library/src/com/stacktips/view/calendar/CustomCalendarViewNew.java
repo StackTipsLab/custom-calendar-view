aa234835480accb4c038ccd9b17b26d79c250c92
Nilanchala Panigrahy <nilanchalapanigrahy@gmail.com>
2016-08-24 18:38:28 +0100

    Updated week layout

--- library/src/com/stacktips/view/calendar/CustomCalendarViewNew.java
+++ library/src/com/stacktips/view/calendar/CustomCalendarViewNew.java
@@ -1,5 +1,6 @@
 package com.stacktips.view.calendar;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Typeface;
@@ -7,6 +8,7 @@ import android.os.Build;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -14,6 +16,7 @@ import android.widget.TextView;
 import com.imanoweb.calendarview.R;
 import com.stacktips.view.CalendarListener;
 import com.stacktips.view.DayDecorator;
+import com.stacktips.view.DayView;
 import com.stacktips.view.utils.Utils;
 
 import java.text.DateFormatSymbols;
@@ -75,6 +78,7 @@ public class CustomCalendarViewNew extends LinearLayout {
         getAttributes(attrs);
 
         initializeCalendar();
+
     }
 
     private void getAttributes(AttributeSet attrs) {
@@ -95,22 +99,143 @@ public class CustomCalendarViewNew extends LinearLayout {
     }
 
     private void initializeCalendar() {
-        initCalendarMonthTitle();
-
-        initCalendarWeeks();
-    }
-
-    private void initCalendarMonthTitle() {
         previousMonthButton = (ImageView) view.findViewById(R.id.leftButton);
         nextMonthButton = (ImageView) view.findViewById(R.id.rightButton);
+        initCalendarWeeks();
+
         // Initialize calendar for current month
         Locale locale = mContext.getResources().getConfiguration().locale;
         Calendar currentCalendar = Calendar.getInstance(locale);
 
-        //setFirstDayOfWeek(Calendar.SUNDAY);
-        //refreshCalendar(currentCalendar);
+        //Default to sunday
+        setFirstDayOfWeek(Calendar.SUNDAY);
+        refreshCalendar(currentCalendar);
     }
 
+
+    public int getFirstDayOfWeek() {
+        return firstDayOfWeek;
+    }
+
+    public void setFirstDayOfWeek(int firstDayOfWeek) {
+        this.firstDayOfWeek = firstDayOfWeek;
+    }
+
+
+    @SuppressLint("DefaultLocale")
+    public void refreshCalendar(Calendar currentCalendar) {
+        this.currentCalendar = currentCalendar;
+        this.currentCalendar.setFirstDayOfWeek(getFirstDayOfWeek());
+        locale = mContext.getResources().getConfiguration().locale;
+
+        // Set date title
+        initializeTitleLayout();
+
+        // Set weeks days titles
+        initializeWeekLayout();
+
+        // Initialize and set days in calendar
+        //setDaysInCalendar();
+    }
+
+
+    /**
+     * Initialize the calendar week layout, considers start day
+     */
+    @SuppressLint("DefaultLocale")
+    private void initializeWeekLayout() {
+        TextView dayOfWeek;
+        String dayOfTheWeekString;
+
+        //Setting background color white
+        View titleLayout = view.findViewById(R.id.weekLayout);
+        titleLayout.setBackgroundColor(weekLayoutBackgroundColor);
+
+        final String[] weekDaysArray = new DateFormatSymbols(locale).getShortWeekdays();
+        for (int i = 1; i < weekDaysArray.length; i++) {
+            dayOfTheWeekString = weekDaysArray[i];
+            if(dayOfTheWeekString.length() > 3){
+                dayOfTheWeekString = dayOfTheWeekString.substring(0, 3).toUpperCase();
+            }
+
+            dayOfWeek = (TextView) view.findViewWithTag(Utils.DAY_OF_WEEK + getWeekIndex(i, currentCalendar));
+            dayOfWeek.setText(dayOfTheWeekString);
+            dayOfWeek.setTextColor(dayOfWeekTextColor);
+
+            if (null != getCustomTypeface()) {
+                dayOfWeek.setTypeface(getCustomTypeface());
+            }
+        }
+    }
+
+
+
+    /*private void setDaysInCalendar() {
+        Calendar calendar = Calendar.getInstance(locale);
+        calendar.setTime(currentCalendar.getTime());
+        calendar.set(Calendar.DAY_OF_MONTH, 1);
+        calendar.setFirstDayOfWeek(getFirstDayOfWeek());
+        int firstDayOfMonth = calendar.get(Calendar.DAY_OF_WEEK);
+
+        // Calculate dayOfMonthIndex
+        int dayOfMonthIndex = getWeekIndex(firstDayOfMonth, calendar);
+        int actualMaximum = calendar.getActualMaximum(Calendar.DAY_OF_MONTH);
+
+        final Calendar startCalendar = (Calendar) calendar.clone();
+        //Add required number of days
+        startCalendar.add(Calendar.DATE, -(dayOfMonthIndex - 1));
+        int monthEndIndex = 42 - (actualMaximum + dayOfMonthIndex - 1);
+
+        DayView dayView;
+        ViewGroup dayOfMonthContainer;
+        for (int i = 1; i < 43; i++) {
+            dayOfMonthContainer = (ViewGroup) view.findViewWithTag(DAY_OF_MONTH_CONTAINER + i);
+            dayView = (DayView) view.findViewWithTag(DAY_OF_MONTH_TEXT + i);
+            if (dayView == null)
+                continue;
+
+            //Apply the default styles
+            dayOfMonthContainer.setOnClickListener(null);
+            dayView.bind(startCalendar.getTime(), getDecorators());
+            dayView.setVisibility(View.VISIBLE);
+
+            if (null != getCustomTypeface()) {
+                dayView.setTypeface(getCustomTypeface());
+            }
+
+            if (isSameMonth(calendar, startCalendar)) {
+                dayOfMonthContainer.setOnClickListener(onDayOfMonthClickListener);
+                dayView.setBackgroundColor(calendarBackgroundColor);
+                dayView.setTextColor(dayOfWeekTextColor);
+                //Set the current day color
+                markDayAsCurrentDay(startCalendar);
+            } else {
+                dayView.setBackgroundColor(disabledDayBackgroundColor);
+                dayView.setTextColor(disabledDayTextColor);
+
+                if (!isOverflowDateVisible())
+                    dayView.setVisibility(View.GONE);
+                else if (i >= 36 && ((float) monthEndIndex / 7.0f) >= 1) {
+                    dayView.setVisibility(View.GONE);
+                }
+            }
+            dayView.decorate();
+
+
+
+            startCalendar.add(Calendar.DATE, 1);
+            dayOfMonthIndex++;
+        }
+
+        // If the last week row has no visible days, hide it or show it in case
+        ViewGroup weekRow = (ViewGroup) view.findViewWithTag("weekRow6");
+        dayView = (DayView) view.findViewWithTag("dayOfMonthText36");
+        if (dayView.getVisibility() != VISIBLE) {
+            weekRow.setVisibility(GONE);
+        } else {
+            weekRow.setVisibility(VISIBLE);
+        }
+    }*/
 
     /**
      * Create dynamically from custom views
@@ -153,40 +278,103 @@ public class CustomCalendarViewNew extends LinearLayout {
         }
 
     }
-/*
-
-    */
-
-    /**
-     * Initialize the calendar week layout, considers start day
-     *//*
-
-    @SuppressLint("DefaultLocale")
-    private void initializeWeekLayout() {
-        TextView dayOfWeek;
-        String dayOfTheWeekString;
-
-        //Setting background color white
-        View titleLayout = view.findViewById(R.id.weekLayout);
-        titleLayout.setBackgroundColor(weekLayoutBackgroundColor);
-
-        final String[] weekDaysArray = new DateFormatSymbols(locale).getShortWeekdays();
-        for (int i = 1; i < weekDaysArray.length; i++) {
-            dayOfTheWeekString = weekDaysArray[i];
-            if (dayOfTheWeekString.length() > 3) {
-                dayOfTheWeekString = dayOfTheWeekString.substring(0, 3).toUpperCase();
+    /*
+        public void markDayAsCurrentDay(Calendar calendar) {
+            if (calendar != null && Utils.isToday(calendar)) {
+                DayView dayOfMonth = getDayOfMonthText(calendar);
+                dayOfMonth.setTextColor(currentDayOfMonth);
             }
+        }
 
-            dayOfWeek = (TextView) view.findViewWithTag(Utils.DAY_OF_WEEK + getWeekIndex(i, currentCalendar));
-            dayOfWeek.setText(dayOfTheWeekString);
-            dayOfWeek.setTextColor(dayOfWeekTextColor);
+        public void markDayAsSelectedDay(Date currentDate) {
+            final Calendar currentCalendar = getTodaysCalendar();
+            currentCalendar.setFirstDayOfWeek(getFirstDayOfWeek());
+            currentCalendar.setTime(currentDate);
 
-            if (null != getCustomTypeface()) {
-                dayOfWeek.setTypeface(getCustomTypeface());
+            // Clear previous marks
+            clearDayOfTheMonthStyle(lastSelectedDay);
+
+            // Store current values as last values
+            storeLastValues(currentDate);
+
+            // Mark current day as selected
+            DayView view = getDayOfMonthText(currentCalendar);
+            view.setBackgroundColor(selectedDayBackground);
+            view.setTextColor(selectedDayTextColor);
+        }
+
+
+
+        private void clearDayOfTheMonthStyle(Date currentDate) {
+            if (currentDate != null) {
+                final Calendar calendar = getTodaysCalendar();
+                calendar.setFirstDayOfWeek(getFirstDayOfWeek());
+                calendar.setTime(currentDate);
+
+                final DayView dayView = getDayOfMonthText(calendar);
+                dayView.setBackgroundColor(calendarBackgroundColor);
+                dayView.setTextColor(dayOfWeekTextColor);
+                dayView.decorate();
+            }
+        }
+
+        private DayView getDayOfMonthText(Calendar currentCalendar) {
+            return (DayView) getView(Utils.DAY_OF_MONTH_TEXT, currentCalendar);
+        }
+    */
+    private int getDayIndexByDate(Calendar currentCalendar) {
+        int monthOffset = getMonthOffset(currentCalendar);
+        int currentDay = currentCalendar.get(Calendar.DAY_OF_MONTH);
+        int index = currentDay + monthOffset;
+        return index;
+    }
+
+    private int getMonthOffset(Calendar currentCalendar) {
+        final Calendar calendar = Calendar.getInstance();
+        calendar.setFirstDayOfWeek(getFirstDayOfWeek());
+        calendar.setTime(currentCalendar.getTime());
+        calendar.set(Calendar.DAY_OF_MONTH, 1);
+
+        int firstDayWeekPosition = calendar.getFirstDayOfWeek();
+        int dayPosition = calendar.get(Calendar.DAY_OF_WEEK);
+
+        if (firstDayWeekPosition == 1) {
+            return dayPosition - 1;
+        } else {
+            if (dayPosition == 1) {
+                return 6;
+            } else {
+                return dayPosition - 2;
             }
         }
     }
-*/
+
+    private int getWeekIndex(int weekIndex, Calendar currentCalendar) {
+        int firstDayWeekPosition = currentCalendar.getFirstDayOfWeek();
+        if (firstDayWeekPosition == 1) {
+            return weekIndex;
+        } else {
+
+            if (weekIndex == 1) {
+                return 7;
+            } else {
+                return weekIndex - 1;
+            }
+        }
+    }
+
+    private View getView(String key, Calendar currentCalendar) {
+        int index = getDayIndexByDate(currentCalendar);
+        View childView = view.findViewWithTag(key + index);
+        return childView;
+    }
+
+    private Calendar getTodaysCalendar() {
+        Calendar currentCalendar = Calendar.getInstance(mContext.getResources().getConfiguration().locale);
+        currentCalendar.setFirstDayOfWeek(getFirstDayOfWeek());
+        return currentCalendar;
+    }
+
     public List<DayDecorator> getDecorators() {
         return decorators;
     }
